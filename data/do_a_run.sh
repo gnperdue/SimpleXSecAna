c8aee16e181062e7a61c44ad920aae2ab41634f8
Gabriel Perdue <perdue@fnal.gov>
2014-05-04 17:49:40 -0500

    Change the "do a run" script to use only CCQE on carbon, hydrogen.

--- data/do_a_run.sh
+++ data/do_a_run.sh
@@ -1,18 +1,30 @@
 #!/bin/sh
 
-# Generic run on oxygen to test the installation.
+if [ -z $XSECSPLINEDIR ]; then
+  echo "No XSECSPLINEDIR environment variable defined! Cannot proceed!"
+  exit 0
+fi
 
-NUMEVT=100
+if [ ! -d $XSECSPLINEDIR ]; then
+  echo "No XSECSPLINEDIR directory exists! Cannot proceed!"
+  exit 0
+fi
+
+NUMEVT=1000
 if [ $# -gt 0 ]; then
   NUMEVT=$1
 fi
 
-if [ ! -f $XSECSPLINEDIR/gxspl-vA-v2.8.0.xml ]; then
+SPLINE="$XSECSPLINEDIR/ccqe_hydrogen_carbon_splines.xml"
+
+if [ ! -f $SPLINE ]; then
   echo "Cross section spline file is missing."
   exit 0
 fi
 
-gevgen -n $NUMEVT -p 14 -t 1000080160 -e 0,10 -r 100 \
+gevgen -n $NUMEVT -p 14 -t 1000060120,1000010010 -e 0,10 -r 100 \
   -f 'x*exp(-x)' \
-  --seed 2989819 --cross-sections $XSECSPLINEDIR/gxspl-vA-v2.8.0.xml >& run_log.txt
+  --seed 2989819 \
+  --cross-sections $SPLINE \
+  --event-generator-list CCQE >& run_log.txt
 
