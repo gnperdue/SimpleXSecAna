aad6477b42e0005d975bb65ea44d538d82519e68
Gabriel Perdue <perdue@fnal.gov>
2014-05-04 19:53:45 -0500

    Also make file lists when we make `gtrac` and `gst` files.

--- data/convert_files.sh
+++ data/convert_files.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
-gntpc -i ./gntp.100.ghep.root -f rootracker
-gntpc -i ./gntp.100.ghep.root -f gst
 
+FIRST=100
+LAST=100
+for i in $(eval echo "{$FIRST..$LAST}")
+do
+  echo "$i"
+  gntpc -i ./gntp.$i.ghep.root -f rootracker
+  gntpc -i ./gntp.$i.ghep.root -f gst
+done
+
+ls -1 *gtrac.root > gtrac_list.txt
+ls -1 *gst.root > gst_list.txt
+
+if test -d ../list; then echo ListOk >& /dev/null ; else mkdir -p ../list; fi
+
+mv gtrac_list.txt ../list
+mv gst_list.txt ../list
