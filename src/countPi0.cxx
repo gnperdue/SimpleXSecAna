f39bccafb44a4d8f5beabe597a8f85da76ab6768
Gabriel Perdue <perdue@fnal.gov>
2014-06-10 11:52:03 -0500

    Work out a bit more, including total cross section over an energy range.

--- src/countPi0.cxx
+++ src/countPi0.cxx
@@ -1,3 +1,4 @@
+#include <map>
 #include <string>
 
 #include <TSystem.h>
@@ -5,6 +6,7 @@
 #include <TTree.h>
 #include <TIterator.h>
 #include <TH2.h>
+#include <TGraph.h>
 
 #include "EVGCore/EventRecord.h"
 #include "GHEP/GHepParticle.h"
@@ -14,6 +16,7 @@
 #include "Messenger/Messenger.h"
 #include "PDG/PDGCodes.h"
 #include "Utils/CmdLnArgParser.h"
+#include "Conventions/Units.h"
 
 using std::string;
 using namespace genie;
@@ -46,13 +49,23 @@ int main(int argc, char ** argv)
     TMath::Min(gOptNEvt, (int)tree->GetEntries()) :
     (int) tree->GetEntries();
 
+  double e[nev], xs[nev];
+  std::map<double, double> exs;
+
   TObjArray * Hlist = new TObjArray(0);
 
   TH1D *h_npi0       = new TH1D("h_npi0","Number of Neutral Pions;Number;Counts",10,0,10); 
   TH1D *h_npipi0only = new TH1D("h_npipi0only","Number of Neutral Pions (no Charged Pions);Number;Counts",10,0,10); 
+  TH1D *h_dsigdE     = new TH1D("h_dsigdE","XSec(E);Energy",100,1,3);
 
   Hlist->Add(h_npi0);
   Hlist->Add(h_npipi0only);
+  Hlist->Add(h_dsigdE);
+
+  double total_xsec = 0;
+  double total_ccpi0_xsec = 0;
+  double smallest_xsec = 1000.0;  // in units of 1e-38 cm^2
+  double largest_xsec = 0.0;  // in units of 1e-38 cm^2
 
   //
   // Loop over all events
@@ -65,6 +78,23 @@ int main(int argc, char ** argv)
     // get the GENIE event
     EventRecord &  event = *(mcrec->event);
     Interaction *in = event.Summary();
+    GHepParticle * nu = event.Probe();
+    const ProcessInfo &proc = in->ProcInfo();
+
+    bool iscc = proc.IsWeakCC();
+    double nue = nu->Energy();
+    double nxsec = event.XSec() / (1E-38 * units::cm2);
+    exs.insert(std::pair<double,double>(nue, nxsec));
+    double weight = nxsec / double(nev);
+    total_xsec += weight;
+    h_dsigdE->Fill(nue, weight);
+#if DEBUG
+    LOG("myAnalysis", pNOTICE)  << "neutrino e = " << nue;
+    LOG("myAnalysis", pNOTICE)  << "      xsec = " << nxsec;
+    LOG("myAnalysis", pNOTICE)  << "   xsec/ev = " << weight;
+#endif
+    if (nxsec < smallest_xsec) smallest_xsec = nxsec;
+    if (nxsec > largest_xsec) largest_xsec = nxsec;
 
     //
     // Loop over all particles in this event
@@ -74,12 +104,14 @@ int main(int argc, char ** argv)
 
     int npi0 = 0;
     int nchgdpi = 0;
+    bool havePi0 = false;
 
     while((p=dynamic_cast<GHepParticle *>(event_iter.Next())))
     {
       if (p->Status() == kIStStableFinalState ) {
         if (p->Pdg() == kPdgPi0) {
           npi0++;
+          if (iscc) havePi0 = true;
         }
         if (p->Pdg() == kPdgPiP ||
             p->Pdg() == kPdgPiM) {
@@ -89,6 +121,7 @@ int main(int argc, char ** argv)
 
     }// end loop over particles	
 
+    if (havePi0) total_ccpi0_xsec += weight;
 
     h_npi0->Fill(npi0);
     if (nchgdpi==0) {
@@ -100,13 +133,33 @@ int main(int argc, char ** argv)
 
   }//end loop over events
 
+  int indx = 0;
+  for (std::map<double,double>::iterator it=exs.begin(); it!=exs.end(); ++it) {
+    e[indx] = it->first;
+    xs[indx] = it->second;
+    ++indx;
+  }
+
+  TGraph* exs_gr = new TGraph(nev, e, xs);
+  Hlist->Add(exs_gr);
+
+  LOG("myAnalysis", pNOTICE) << "total xsec = " << total_xsec;
+  LOG("myAnalysis", pNOTICE) << "total xsec by integral = " << h_dsigdE->Integral();
+  LOG("myAnalysis", pNOTICE) << "smallest observed xsec = " << smallest_xsec;
+  LOG("myAnalysis", pNOTICE) << "largest observed xsec = " << largest_xsec;
+  LOG("myAnalysis", pNOTICE) << "total ccpi0 = " << total_ccpi0_xsec;
+
+  // save histograms
   std::string histname = "$ANA_HIST_DIR/countPi0.root";
   TFile * outputfile = new TFile(histname.c_str(),"RECREATE");
   Hlist->Write();
   outputfile->Close();
 
+  // clean up memory
   delete h_npi0;
   delete h_npipi0only;
+  delete h_dsigdE;
+  delete exs_gr;
   delete Hlist;
 
   // close input GHEP event file
