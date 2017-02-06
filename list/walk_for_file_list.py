08c2f1a4e8994ed6c8cf8e9f82e35b9e42c4a744
Gabriel Perdue <perdue@fnal.gov>
2017-02-06 15:09:26 -0600

    cli options, sort of, more verbose search process

--- list/walk_for_file_list.py
+++ list/walk_for_file_list.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
+"""
+Get a list of files. The script makes assumptions about the path and file
+extension that are probably not what you want.
 
+Usage:
+    python walk_for_file_list.py /path/to/files filename-extension out-file
+
+One may optionally supply the args, but note the expected position, etc.
+"""
 from __future__ import print_function
 import os
+import sys
 
 
 def enumerate_files(path, keystring):
@@ -28,11 +37,30 @@ def write_list_of_files_to_file(list_of_files, filenam):
 
 if __name__ == '__main__':
 
+    if '-h' in sys.argv or '--help' in sys.argv:
+        print(__doc__)
+        sys.exit(1)
+
     data_path  = '/pnfs/minerva/scratch/users/minervapro'
     data_path += '/mc_production_genie_DFR_v10r8p4/grid/central_value/minerva'
     data_path += '/genie/v10r8p4/00/01/00/'
 
-    search_string = r'(.*)v10r8p4_DFR_ghep.root'
+    if len(sys.argv) > 1:
+        data_path = sys.argv[1]
 
-    files = enumerate_files(data_path, search_string)
-    write_list_of_files_to_file(files, 'diffractive_events_file_list.txt')
+    search_extension = r'(.*)v10r8p4_DFR_ghep.root'
+
+    if len(sys.argv) > 2:
+        search_extension = sys.argv[2]
+
+    out_file = 'events_file_list.txt'
+
+    if len(sys.argv) > 3:
+        out_file = sys.argv[3]
+
+    print('data path:', data_path)
+    print('search extension:', search_extension)
+    print('out file:', out_file)
+
+    files = enumerate_files(data_path, search_extension)
+    write_list_of_files_to_file(files, out_file)
