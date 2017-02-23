0214cb02630a2ffd8a0da29527351b1e72088e3d
Gabriel Perdue <perdue@fnal.gov>
2017-02-23 16:38:32 -0600

    misc working stuff

--- run/run_qetotalxsec.sh
+++ run/run_qetotalxsec.sh
@@ -6,18 +6,18 @@ DEBUG=""
 NEVT=-1
 NEVT=50000
 
-$DEBUG ../bin/qetotalxsec -f default_nubar_qe_like_scint.txt \
-    -o totalxsec_default_nubar_qe_like_scint.root \
-    -m $NEVT
+# $DEBUG ../bin/qetotalxsec -f default_nubar_qe_like_scint.txt \
+#     -o totalxsec_default_nubar_qe_like_scint.root \
+#     -m $NEVT
 
-$DEBUG ../bin/qetotalxsec -f default_nu_qe_like_scint.txt \
-    -o totalxsec_default_nu_qe_like_scint.root \
-    -m $NEVT
+# $DEBUG ../bin/qetotalxsec -f default_nu_qe_like_scint.txt \
+#     -o totalxsec_default_nu_qe_like_scint.root \
+#     -m $NEVT
 
 $DEBUG ../bin/qetotalxsec -f default_nu_qe_like_carbon.txt \
     -o totalxsec_default_nu_qe_like_carbon.root \
     -m $NEVT
 
-$DEBUG ../bin/qetotalxsec -f default_nu_qe_like_iron.txt \
-    -o totalxsec_default_nu_qe_like_iron.root \
-    -m $NEVT
+# $DEBUG ../bin/qetotalxsec -f default_nu_qe_like_iron.txt \
+#     -o totalxsec_default_nu_qe_like_iron.root \
+#     -m $NEVT
