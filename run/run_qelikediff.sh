669619eac774d05068279d9998212ebcac1a385d
Gabriel Perdue <perdue@fnal.gov>
2017-02-28 17:18:20 -0600

    add options (sort of) to process more files

--- run/run_qelikediff.sh
+++ run/run_qelikediff.sh
@@ -3,21 +3,21 @@
 DEBUG="gdb -tui --args "
 DEBUG=""
 
-nubarfilelistlist="
-default_nubar_qe_like_scint.txt
-zexp_nubar_qe_like_scint.txt
-"
+# nubarfilelistlist="
+# default_nubar_qe_like_scint.txt
+# zexp_nubar_qe_like_scint.txt
+# "
 
-nufilelistlist="
-default_nu_qe_like_scint.txt
-zexp_nu_qe_like_scint.txt
-default_nu_qe_like_carbon.txt
-zexp_nu_qe_like_carbon.txt
-default_nu_qe_like_iron.txt
-zexp_nu_qe_like_iron.txt
-default_nu_qe_like_lead.txt
-zexp_nu_qe_like_lead.txt
-"
+# nufilelistlist="
+# default_nu_qe_like_scint.txt
+# zexp_nu_qe_like_scint.txt
+# default_nu_qe_like_carbon.txt
+# zexp_nu_qe_like_carbon.txt
+# default_nu_qe_like_iron.txt
+# zexp_nu_qe_like_iron.txt
+# default_nu_qe_like_lead.txt
+# zexp_nu_qe_like_lead.txt
+# "
 
 NEVT=-1
 NEVT=100000
@@ -30,8 +30,57 @@ $DEBUG ../bin/qelikediff -f default_nubar_qe_like_scint.txt \
     -n 0 -x 50 -c 13 -u 13
     # -n 1.5 -x 10 -c 7 -u 13
 
+# $DEBUG ../bin/qelikediff -f zexp_nubar_qe_like_scint.txt \
+#     -o zexp_nubar_qe_like_scint.root \
+#     -m $NEVT \
+#     -s -14 \
+#     -n 0 -x 50 -c 13 -u 13
+
 $DEBUG ../bin/qelikediff -f default_nu_qe_like_scint.txt \
     -o default_nu_qe_like_scint.root \
     -m $NEVT \
     -s 14 \
     -n 0 -x 50 -c 13 -u 13
+
+# $DEBUG ../bin/qelikediff -f default_nu_qe_like_carbon.txt \
+#     -o default_nu_qe_like_carbon.root \
+#     -m $NEVT \
+#     -s 14 \
+#     -n 0 -x 50 -c 12 -u 12
+
+# $DEBUG ../bin/qelikediff -f default_nu_qe_like_iron.txt \
+#     -o default_nu_qe_like_iron.root \
+#     -m $NEVT \
+#     -s 14 \
+#     -n 0 -x 50 -c 56 -u 56
+
+# $DEBUG ../bin/qelikediff -f default_nu_qe_like_lead.txt \
+#     -o default_nu_qe_like_lead.root \
+#     -m $NEVT \
+#     -s 14 \
+#     -n 0 -x 50 -c 207 -u 207
+
+# $DEBUG ../bin/qelikediff -f zexp_nu_qe_like_scint.txt \
+#     -o zexp_nu_qe_like_scint.root \
+#     -m $NEVT \
+#     -s 14 \
+#     -n 0 -x 50 -c 13 -u 13
+
+# $DEBUG ../bin/qelikediff -f zexp_nu_qe_like_carbon.txt \
+#     -o zexp_nu_qe_like_carbon.root \
+#     -m $NEVT \
+#     -s 14 \
+#     -n 0 -x 50 -c 12 -u 12
+
+# $DEBUG ../bin/qelikediff -f zexp_nu_qe_like_iron.txt \
+#     -o zexp_nu_qe_like_iron.root \
+#     -m $NEVT \
+#     -s 14 \
+#     -n 0 -x 50 -c 56 -u 56
+
+# $DEBUG ../bin/qelikediff -f zexp_nu_qe_like_lead.txt \
+#     -o zexp_nu_qe_like_lead.root \
+#     -m $NEVT \
+#     -s 14 \
+#     -n 0 -x 50 -c 207 -u 207
+
