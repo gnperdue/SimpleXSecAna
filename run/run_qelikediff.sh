9367c3270dc470d6f7e7b2817c4f3e82ec39148c
Gabriel Perdue <perdue@fnal.gov>
2017-03-01 16:22:31 -0600

    don't keep a ccqe spline around, who knows how old it is

--- run/run_qelikediff.sh
+++ run/run_qelikediff.sh
@@ -30,11 +30,11 @@ $DEBUG ../bin/qelikediff -f default_nubar_qe_like_scint.txt \
     -n 0 -x 50 -c 13 -u 13
     # -n 1.5 -x 10 -c 7 -u 13
 
-# $DEBUG ../bin/qelikediff -f zexp_nubar_qe_like_scint.txt \
-#     -o zexp_nubar_qe_like_scint.root \
-#     -m $NEVT \
-#     -s -14 \
-#     -n 0 -x 50 -c 13 -u 13
+$DEBUG ../bin/qelikediff -f zexp_nubar_qe_like_scint.txt \
+    -o zexp_nubar_qe_like_scint.root \
+    -m $NEVT \
+    -s -14 \
+    -n 0 -x 50 -c 13 -u 13
 
 $DEBUG ../bin/qelikediff -f default_nu_qe_like_scint.txt \
     -o default_nu_qe_like_scint.root \
@@ -42,45 +42,45 @@ $DEBUG ../bin/qelikediff -f default_nu_qe_like_scint.txt \
     -s 14 \
     -n 0 -x 50 -c 13 -u 13
 
-# $DEBUG ../bin/qelikediff -f default_nu_qe_like_carbon.txt \
-#     -o default_nu_qe_like_carbon.root \
-#     -m $NEVT \
-#     -s 14 \
-#     -n 0 -x 50 -c 12 -u 12
+$DEBUG ../bin/qelikediff -f default_nu_qe_like_carbon.txt \
+    -o default_nu_qe_like_carbon.root \
+    -m $NEVT \
+    -s 14 \
+    -n 0 -x 50 -c 12 -u 12
 
-# $DEBUG ../bin/qelikediff -f default_nu_qe_like_iron.txt \
-#     -o default_nu_qe_like_iron.root \
-#     -m $NEVT \
-#     -s 14 \
-#     -n 0 -x 50 -c 56 -u 56
+$DEBUG ../bin/qelikediff -f default_nu_qe_like_iron.txt \
+    -o default_nu_qe_like_iron.root \
+    -m $NEVT \
+    -s 14 \
+    -n 0 -x 50 -c 56 -u 56
 
-# $DEBUG ../bin/qelikediff -f default_nu_qe_like_lead.txt \
-#     -o default_nu_qe_like_lead.root \
-#     -m $NEVT \
-#     -s 14 \
-#     -n 0 -x 50 -c 207 -u 207
+$DEBUG ../bin/qelikediff -f default_nu_qe_like_lead.txt \
+    -o default_nu_qe_like_lead.root \
+    -m $NEVT \
+    -s 14 \
+    -n 0 -x 50 -c 207 -u 207
 
-# $DEBUG ../bin/qelikediff -f zexp_nu_qe_like_scint.txt \
-#     -o zexp_nu_qe_like_scint.root \
-#     -m $NEVT \
-#     -s 14 \
-#     -n 0 -x 50 -c 13 -u 13
+$DEBUG ../bin/qelikediff -f zexp_nu_qe_like_scint.txt \
+    -o zexp_nu_qe_like_scint.root \
+    -m $NEVT \
+    -s 14 \
+    -n 0 -x 50 -c 13 -u 13
 
-# $DEBUG ../bin/qelikediff -f zexp_nu_qe_like_carbon.txt \
-#     -o zexp_nu_qe_like_carbon.root \
-#     -m $NEVT \
-#     -s 14 \
-#     -n 0 -x 50 -c 12 -u 12
+$DEBUG ../bin/qelikediff -f zexp_nu_qe_like_carbon.txt \
+    -o zexp_nu_qe_like_carbon.root \
+    -m $NEVT \
+    -s 14 \
+    -n 0 -x 50 -c 12 -u 12
 
-# $DEBUG ../bin/qelikediff -f zexp_nu_qe_like_iron.txt \
-#     -o zexp_nu_qe_like_iron.root \
-#     -m $NEVT \
-#     -s 14 \
-#     -n 0 -x 50 -c 56 -u 56
+$DEBUG ../bin/qelikediff -f zexp_nu_qe_like_iron.txt \
+    -o zexp_nu_qe_like_iron.root \
+    -m $NEVT \
+    -s 14 \
+    -n 0 -x 50 -c 56 -u 56
 
-# $DEBUG ../bin/qelikediff -f zexp_nu_qe_like_lead.txt \
-#     -o zexp_nu_qe_like_lead.root \
-#     -m $NEVT \
-#     -s 14 \
-#     -n 0 -x 50 -c 207 -u 207
+$DEBUG ../bin/qelikediff -f zexp_nu_qe_like_lead.txt \
+    -o zexp_nu_qe_like_lead.root \
+    -m $NEVT \
+    -s 14 \
+    -n 0 -x 50 -c 207 -u 207
 
